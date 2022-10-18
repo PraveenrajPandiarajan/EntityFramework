c953d34b7789f9549a14833b28d7865aa342b1ce
STS670-PRAVEENRAJ P <praveenrajp199911@gmail.com>
2022-10-18 16:49:43 +0530

    Add project files.

---
+++ databases/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using databases.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication;
+
+namespace databases.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        DetailsContext _taskContext;
+        public HomeController(ILogger<HomeController> logger, DetailsContext taskContext)
+        {
+            _logger = logger;
+            this._taskContext = taskContext;
+        }
+     
+   
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Mod(StudentDetails user)
+        {
+            StudentDetails studentDetails = new StudentDetails();
+           
+            studentDetails.Address = user.Address;
+            studentDetails.FirstName = user.FirstName;
+            studentDetails.LastName = user.LastName;
+            studentDetails.EmailId = user.EmailId;
+            studentDetails.StudentId = user.StudentId;
+            _taskContext.Add(studentDetails);
+            _taskContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ databases/Models/detail.cs
@@ -0,0 +1,11 @@
+﻿namespace databases.Models
+{
+    public class detail
+    {
+        public string? FirstName { get; set; }
+        public  string? LastName { get; set; }
+        public string? EmailId { get; set; }
+        public string Address { get; set; }
+        public int studentId { get; set; }
+    }
+}
